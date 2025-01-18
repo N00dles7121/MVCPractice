85e4374c9f7ff5f8ce1bf44023f12ca036e68d38
Albert Kargaev Laptop <n00dles7121@gmail.com>
2025-01-18 12:22:42 -0500

    Remove unnecessary whitespace in Category model

--- Models/Category.cs
+++ Models/Category.cs
@@ -8,7 +8,6 @@ namespace MVCPractice.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        
         public int DisplayOrder { get; set; }
     }
 }
