2194d1175cbee2aca0f29823ac11483f31b74650
Albert Kargaev Laptop <n00dles7121@gmail.com>
2025-01-18 21:06:52 -0500

    Added validation to Category model

--- Models/Category.cs
+++ Models/Category.cs
@@ -7,7 +7,9 @@ namespace MVCPractice.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+        [Range(0, 100)]
         public int DisplayOrder { get; set; }
     }
 }
