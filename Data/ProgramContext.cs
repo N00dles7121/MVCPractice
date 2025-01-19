8140b992846a1a012887a74cb662c8cc024703c0
Albert Kargaev Laptop <n00dles7121@gmail.com>
2025-01-18 20:20:10 -0500

    Categories table populated with data

--- Data/ProgramContext.cs
+++ Data/ProgramContext.cs
@@ -15,5 +15,29 @@ namespace MVCPractice.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "SciFi",
+                    DisplayOrder = 1
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Fantasy",
+                    DisplayOrder = 2
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Historical",
+                    DisplayOrder = 3
+                }
+            );
+        }
     }
 }
