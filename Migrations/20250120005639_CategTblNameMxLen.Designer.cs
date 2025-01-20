830aadb4252a101da8ae0c02865399ee57b6c0d9
Albert Kargaev Laptop <n00dles7121@gmail.com>
2025-01-19 19:58:53 -0500

    Add migration for Category table name max length

--- Migrations/20250120005639_CategTblNameMxLen.Designer.cs
+++ Migrations/20250120005639_CategTblNameMxLen.Designer.cs
@@ -3,6 +3,7 @@ using MVCPractice.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MVCPractice.Migrations
 {
     [DbContext(typeof(ProgramContext))]
-    partial class ProgramContextModelSnapshot : ModelSnapshot
+    [Migration("20250120005639_CategTblNameMxLen")]
+    partial class CategTblNameMxLen
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
