2061e6fb836e7e0e3e9d6bef44c61fcf0a379802
Albert Kargaev Laptop <albertkargaev7121@gmail.com>
2025-01-22 20:26:18 -0500

    Add Async suffix on methods and add overload for updating by ID

--- MVCPractice.Data/Repositories/CategoryRepo.cs
+++ MVCPractice.Data/Repositories/CategoryRepo.cs
@@ -15,9 +15,22 @@ public class CategoryRepo : Repo<Category>
         _dbSet = _context.Set<Category>();
     }
 
-    public override async Task Update(Category entity)
+    public override async Task UpdateAsync(Category entity)
     {
         if (entity == null || await _dbSet.FindAsync(entity) == null)
+        {
+            throw new Exception("Entity not found");
+        }
+
+        _dbSet.Update(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public override async Task UpdateAsync(int id)
+    {
+        var entity = await _dbSet.FindAsync(id);
+
+        if (entity == null)
         {
             throw new Exception("Entity not found");
         }
