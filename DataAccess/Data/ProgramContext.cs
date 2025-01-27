5ddf361f76482a3e3903d12235d5438c580f8498
Albert Kargaev Laptop <albertkargaev7121@gmail.com>
2025-01-26 19:42:39 -0500

    Add DdSet props for db tables

--- DataAccess/Data/ProgramContext.cs
+++ DataAccess/Data/ProgramContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Models;
 using MVCPractice.Models;
 
 namespace MVCPractice.DataAccess.Data;
@@ -7,4 +8,11 @@ public class ProgramContext : DbContext
     public ProgramContext(DbContextOptions<ProgramContext> options) : base(options) { }
 
     public DbSet<Category> Categories { get; init; }
+    public DbSet<Movie> Movies { get; init; }
+    public DbSet<Actor> Actors { get; init; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProgramContext).Assembly);
+    }
 }
