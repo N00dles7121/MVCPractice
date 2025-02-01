33b80c45c6efcce212c7fad2459b676635765905
Albert Kargaev Laptop <albertkargaev7121@gmail.com>
2025-01-31 20:20:20 -0500

    Remove unused field from CategoryRepo constructor

--- DataAccess/Repositories/CategoryRepo.cs
+++ DataAccess/Repositories/CategoryRepo.cs
@@ -5,11 +5,8 @@ namespace DataAccess.Repositories
 {
     public class CategoryRepo : Repo<Category>
     {
-        private readonly ProgramContext _context;
-
         public CategoryRepo(ProgramContext context) : base(context)
         {
-            _context = context;
         }
     }
 }
