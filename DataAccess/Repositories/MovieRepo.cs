4b237b907e057b1764906f43b64915e2e2a5ef15
Albert Kargaev Laptop <albertkargaev7121@gmail.com>
2025-01-31 22:55:13 -0500

    Add update method with use of MovieDTO class

--- DataAccess/Repositories/MovieRepo.cs
+++ DataAccess/Repositories/MovieRepo.cs
@@ -1,12 +1,29 @@
 using System;
+using Microsoft.EntityFrameworkCore;
 using Models;
+using Models.DTOs;
 using MVCPractice.DataAccess.Data;
 
 namespace DataAccess.Repositories;
 
 public class MovieRepo : Repo<Movie>
 {
+    private readonly ProgramContext _context;
+
     public MovieRepo(ProgramContext context) : base(context)
     {
+        _context = context;
+    }
+
+    public async Task ExecuteUpdateAsync(MovieDTO movieDto, int id)
+    {
+        var affectedRows = await _context.Movies.Where(m => m.Id == id).ExecuteUpdateAsync(s => s
+        .SetProperty(m => m.Title, movieDto.Title)
+        .SetProperty(m => m.ReleaseDate, movieDto.ReleaseDate)
+        .SetProperty(m => m.Rating, movieDto.Rating)
+        .SetProperty(m => m.CategoryId, movieDto.CategoryId));
+        // .SetProperty(m => m.Actors, movieDto.Actors));
+
+        await _context.SaveChangesAsync();
     }
 }
