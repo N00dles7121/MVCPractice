5a91e85d31fe78ac9bb0cee8a3af854f3df7b281
Albert Kargaev Laptop <albertkargaev7121@gmail.com>
2025-02-01 21:27:37 -0500

    add ExecuteUpdate method to use DTO for update

--- DataAccess/Repositories/CategoryRepo.cs
+++ DataAccess/Repositories/CategoryRepo.cs
@@ -1,12 +1,26 @@
 using MVCPractice.DataAccess.Data;
 using Models;
+using Models.DTOs;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Repositories
 {
     public class CategoryRepo : Repo<Category>
     {
+        private readonly ProgramContext _context;
+
         public CategoryRepo(ProgramContext context) : base(context)
         {
+            _context = context;
+        }
+
+        public async Task ExecuteUpdateAsync(CategoryDTO dto, int id)
+        {
+            await _context.Categories.Where(m => m.Id == id).ExecuteUpdateAsync(s => s
+            .SetProperty(m => m.Name, dto.Name)
+            .SetProperty(m => m.DisplayOrder, dto.DisplayOrder));
+
+            await _context.SaveChangesAsync();
         }
     }
 }
